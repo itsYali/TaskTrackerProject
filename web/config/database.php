44a08617163a6766cdb22bd63b3043ed3ded80f0
unknown <eria@lambourne.org>
2025-06-12 04:18:02 +0800

    added an attempt to create a sql db if not exist

--- web/config/database.php
+++ web/config/database.php
@@ -5,8 +5,10 @@ $username = 'root';
 $password = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("CREATE DATABASE IF NOT EXISTS $dbname");
+    $pdo->exec("USE $dbname");
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
