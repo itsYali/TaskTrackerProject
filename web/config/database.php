46ec3c913777e469909fca55986c34ab593a30d9
unknown <eria@lambourne.org>
2025-06-12 04:30:21 +0800

    undid last change

--- web/config/database.php
+++ web/config/database.php
@@ -1,14 +1,12 @@
 <?php
-$host = '0.0.0.0';
+$host = 'localhost';
 $dbname = 'task_tracker';
 $username = 'root';
 $password = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->exec("CREATE DATABASE IF NOT EXISTS $dbname");
-    $pdo->exec("USE $dbname");
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
