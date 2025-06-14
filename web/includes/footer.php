2cf9526f8087717791293879e53bdcabdc6f35d8
Lan Yali <eria@lambourne.org>
2025-06-14 21:43:52 +0800

    update includes

--- web/includes/footer.php
+++ web/includes/footer.php
@@ -1,6 +1,6 @@
     </main>
     <footer>
-        <p>Open Source Practice Project: Task Tracker Demo</p>
+        <p>Open Source Practice Project: Task Tracker</p>
     </footer>
     <script src="javascript/main.js"></script>
 </body>
